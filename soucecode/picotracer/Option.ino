99af296c78ad590fb4e5c3085a34f4e299c013f9
kein <ken510073@gmail.com>
2025-11-09 13:36:33 +0900

    update/Scene4：テスト用のモータ動作の変更Scene5：不要部分の削除やみたい変数の追加などScene6：無効化部分の解除
    ・Scene4：モーターの動作を加速テーブルをベースにした制御に変更したため、それの対応を実施
    ・Scene5：センサー試験において必要ない処理の削除とみたい変数の追加
    ・Scene6：ログ出力機能のコメント解除とログ出力中にモーターが回転しないような処理の追加

--- soucecode/picotracer/Option.ino
+++ soucecode/picotracer/Option.ino
@@ -9,31 +9,27 @@
 //@details　ハードウェアを変更した時にモーターが動作するかを確認するようの関数
 //---------------------------------------------------------------------
 void Scene4() {
-  //PWM
-//  pwm_set_wrap(pwm_slice1, Hz_wrap(Speed));
-//  pwm_set_wrap(pwm_slice2, Hz_wrap(Speed));
-
-  Speed=Speed + 1;
-  if(Speed>1500){
-    Speed=1800;
+  static bool a = 0;
+  if(interval <= 1000 && a == 0){
+    interval_tar = 999;
   }
-//  pwm_set_enabled(pwm_slice1, true);
-//  pwm_set_enabled(pwm_slice2, true);
-StepSW = 1;
-    
-  intervalR = frequencyToInterval(500);
-  intervalL = frequencyToInterval(250);
- Serial.print(" Step_L:");
- Serial.print(Step_L);
- Serial.print(" Step_R:");
- Serial.print(Step_R);
- Serial.print("Direction:");
- Serial.println(Get_Distance(Step_L,Step_R));
-   if (Get_Distance(Step_L,Step_R) > 300) {  //停止位置は試走会で調整
+  if(interval == 999){
+    a=1;
+    interval_tar = 0;
+  }
+  // if(a == 1){
+  // interval_tar = 0;
+  // }
+  Serial.print("StepL:");
+  Serial.println(Step_L);
+  // Serial.print("StepR:");
+  // Serial.println(Step_R);
+  
+  // if(NowDistance > 1000){
+  //   digitalWrite(ENABLE_L, HIGH);
+  //   digitalWrite(ENABLE_R, HIGH);
+  // }
 
-      Reset();
-      Run = 0;
-    }
 }
 
 //---------------------------------------------------------------------
@@ -51,110 +47,45 @@ void Scene5() {
   //センサー値出力オンオフ管理変数
   static bool sensor_out=true;
 
-//実際には動かないが、Step上動作させるために定義
-  intervalR = frequencyToInterval(10);//R
-  intervalL = frequencyToInterval(10);//L
-  //センサー値出力ONOFF切り替え
-  if(sw1 == 1) {
-    sensor_out= !sensor_out;
-    delay(1000);
-  }
+// //実際には動かないが、Step上動作させるために定義
+//   interval = frequencyToInterval(10);//R
+//   intervalL = frequencyToInterval(10);//L
+//   //センサー値出力ONOFF切り替え
+//   if(sw1 == 1) {
+//     sensor_out= !sensor_out;
+//     delay(1000);
+//   }
 
-  //クロスラインを検知したら、クロスフラグをON
-  if (tmpc == 0 && sensorL < 300 && sensorR < 300 && sensorLL < 300 && sensorRR < 300) {
-    temp_distance = NowDistance;
-    cross = 1;
-    tmpc = 1;
-    }
-  if ( tmpc == 1 && sensorLL > 700 && sensorRR > 700) {
-    tmpc = 0;
-  }
+//   static uint16_t tmp_distance = 0;
+//   static uint8_t index = 0;
 
-  //ゴールセンサが検知したらtmpフラグをON
-  if (tmp == 0 && sensorGoal < 300 ){  //速度によって調整  && sensorRR > 500 && sensorLL > 500 && Curve > 800
-    if(cross != 1){
-      count++;
-      //BUZZER入れたが音小さくて聞こえない
-      tone(BUZZER,1178,100);
-      StepSW = !StepSW;
-    }
-    tmp = 1;
-  }
-  //ゴールセンサが検知しなくなったらクロスフラグを確認し、ゴールマーカーを追加するか判断
-  if (tmp == 1 && sensorGoal > 700) {
-    tmp = 0;
-  }
+  sensorCurve = analogRead(Curve_Sensor);
+  sensorLL = read_adc(ch1, SELPIN1);
+  sensorL = read_adc(ch0, SELPIN1);
+  sensorR = read_adc(ch1, SELPIN2);
+  sensorRR = read_adc(ch0, SELPIN2);
+  sensorGoal = analogRead(GOALSENSOR);
 
-  // // //カーブマーカー検知
-  // if (curve_temp == 0  && sensorCurve < 300){
-  //     if(cross != 1){
-  //     curve_count++;
-  //     //BUZZER入れたが音小さくて聞こえない
-  //     tone(BUZZER,1178,100);
-  //   }
-  // }
-  // if (sensorCurve > 700) {
-  //   curve_temp = 0;
-  // }
-  //クロス検知後から35mm進んだらおそらくゴール、カーブセンサは過ぎてるだろうからクロスフラグを戻す
-  if((NowDistance-temp_distance) >= 35){
-    cross = 0;
-  }
-
-  
-
-  if(c == 0){
-    //基準速度
-    SP = 0;
-    //Pゲイン
-    pgain = 0.34;
-    //Dゲイン
-    dgain = 0.001;
-    //Iゲイン
-    igain = 0.0;
-    // 1回しか入らないようcを1にする
-    c=1;
-  }
-
-  Add_SensorL = sensorL + sensorLL;
-  Add_SensorR = sensorR + sensorRR;
-
-  //今回の差分
-  diff = Add_SensorL - Add_SensorR - bias;  //biasは試走会で調整
-
-  P = diff * pgain;
-  //D制御
-  D = (beforediff - diff)/0.01 * dgain;
-  
-    //-50~50なら差分なしとする（）
-  if(diff<=650 && diff>=-650){
-    diff=0;
-  }
 
   if(sensor_out==true){
-    sensorCurve = analogRead(Curve_Sensor);
+    
     Serial.print(sensorCurve, DEC);
     Serial.print(" ");
-    sensorLL = read_adc(ch1, SELPIN2);//sensor ll
     Serial.print(sensorLL, DEC);
     Serial.print(" ");
-    sensorL = read_adc(ch0, SELPIN2)-10;//sensor l
     Serial.print(sensorL, DEC);
     Serial.print(" ");
-    sensorR = read_adc(ch1, SELPIN1)-10;//sensor r
     Serial.print(sensorR, DEC);
     Serial.print(" ");
-    sensorRR = read_adc(ch0, SELPIN1)+15;//sensor rr
     Serial.print(sensorRR, DEC);
     Serial.print(" ");
-    sensorGoal = analogRead(GOALSENSOR);
     Serial.print(sensorGoal, DEC);
     Serial.print("  count=");
     Serial.print(count, DEC);
     Serial.print(" cross=");
     Serial.print(cross, DEC);
-    Serial.print(" tmpc=");
-    Serial.print(tmpc, DEC);
+    Serial.print(" curve_count=");
+    Serial.print(curve_count, DEC);
     Serial.print(" tmp=");
     Serial.print(tmp, DEC);
     Serial.print(" NowDistance=");
@@ -177,22 +108,29 @@ void Scene5() {
 //---------------------------------------------------------------------
 void Scene6(){
   static uint16_t cou = 0;
+    //モーター電源オフ
+  digitalWrite(ENABLE_L, HIGH);
+  digitalWrite(ENABLE_R, HIGH);
+    for(int z=0;z<=71;z++){
+      Serial.print("marker_distance：");
+      Serial.println(marker_distance[z]);
+    }
 
-  // Serial.print(data_log[cou].Curve_log);
-  // Serial.print("  ,  ");
-  // Serial.print(data_log[cou].LL_log);
-  // Serial.print("  ,  ");
-  // Serial.print(data_log[cou].L_log);
-  // Serial.print("  ,  ");
-  // Serial.print(data_log[cou].R_log);
-  // Serial.print(",  ");
-  // Serial.print(data_log[cou].RR_log);
-  // Serial.print("  ,  ");
-  // Serial.print(data_log[cou].Goal_log);
-  // Serial.print("  ,    "); 
+  Serial.print(data_log[cou].Curve_log);
+  Serial.print("  ,  ");
+  Serial.print(data_log[cou].LL_log);
+  Serial.print("  ,  ");
+  Serial.print(data_log[cou].L_log);
+  Serial.print("  ,  ");
+  Serial.print(data_log[cou].R_log);
+  Serial.print(",  ");
+  Serial.print(data_log[cou].RR_log);
+  Serial.print("  ,  ");
+  Serial.print(data_log[cou].Goal_log);
+  Serial.print("  ,    "); 
 
   Serial.print(cou);
-  Serial.print(":");
+  Serial.print(" markerdistance:");
   Serial.println(marker_distance[cou]);
 
   cou++;
