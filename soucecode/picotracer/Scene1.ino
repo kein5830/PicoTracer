e567dfa524cf9c1e1767cbabbc08d03bb1bf6e90
kein <ken510073@gmail.com>
2025-11-09 14:25:14 +0900

    add/マーカを元に加減速走行する機能の実装、不要コメントの削除など
    ・１走目でカーブマーカ間の走行距離を取得し、２走目のマーカー間で1000mm以上の距離であれば、加減速を実施するように処理

--- soucecode/picotracer/Scene1.ino
+++ soucecode/picotracer/Scene1.ino
@@ -2,230 +2,204 @@
 #include "types.h"
 
 //---------------------------------------------------------------------
-//@fn　走行関数Scene1　＞＞＞旧方式でのモータ制御
+//@fn　走行関数Scene1　
 //@brief ライントレース走行
 //@param なし
 //@return void
 //@details　P制御のみ　スタート時のゆっくり加速あり+1　最高速度850
 //---------------------------------------------------------------------
 void Scene1() {
-//  //ログ保存用構造体　宣言
-//  static Log data_log[10000];
-/*
-限界感度法
-P=0.6* 0.34=0.204
-D=0.125*0.075=0.009375
-I=0.5*0.075=0.0375
-
-P=0.45*0.34=0.153
-I=0.83*0.075=0.06225
-*/
+ //*******************************************************************
+  //メイン走行スピード、PIDのゲイン値、その他ローカル変数定義
+  //*******************************************************************
+  //初期
+    static uint16_t tmp_distance = 0;
+    static uint32_t temp_distance2 = 1;
+    static int tmporary = 0;
   if(c == 0){
     //基準速度
-    SP = 0;
+    interval_tar = 270;
     //Pゲイン
-    pgain = 0.38;
+    pgain = 0.3;
     //Dゲイン
-    dgain = 0.000;
+    dgain = 0;
     //Iゲイン
     igain = 0.0;
-    // 1回しか入らないようcを1にする
+    
     c=1;
   }
+  pgain = 0.3;
 
-//センサー配置修正12/17
+  //*******************************************************************
+  //センサー値格納　（202412月17日に配置更新）
+  //*******************************************************************
   sensorCurve = analogRead(Curve_Sensor);
-  sensorLL = read_adc(ch1, SELPIN1);//sensor ll
-  sensorL = read_adc(ch0, SELPIN1)-10;//sensor l
-  sensorR = read_adc(ch1, SELPIN2)-10;//sensor r
-  sensorRR = read_adc(ch0, SELPIN2)+20;//sensor rr
+  sensorLL = read_adc(ch1, SELPIN1);
+  sensorL = read_adc(ch0, SELPIN1);
+  sensorR = read_adc(ch1, SELPIN2);
+  sensorRR = read_adc(ch0, SELPIN2);
   sensorGoal = analogRead(GOALSENSOR);
 
-//  Serial.print(Curve, DEC);
-//  Serial.print(" ");
-//  Serial.print(sensorLL, DEC)
-//  Serial.print(" ");
-//  Serial.print(sensorL, DEC);
-//  Serial.print(" "); 
-//  Serial.print(sensorR, DEC);
-//  Serial.print(" ");
-//  Serial.print(sensorRR, DEC);
-//  Serial.print(" ");
-//  Serial.print(sensorGoal, DEC);
-//  Serial.print(" ");
+  //必要であれば
+  // Serial.print(marker_distance[curve_count], DEC);
+  // Serial.print(" ");
+  // Serial.print(NowDistance, DEC);
+  // Serial.print(" ");
+  // Serial.print(temp_distance2, DEC);
+  // Serial.print(" "); 
+  // Serial.print(curve_count, DEC);
+  // // Serial.print(" ");
+  // // Serial.print(sensorRR, DEC);
+  // // Serial.print(" ");
+  // // Serial.print(sensorGoal, DEC);
+  // Serial.println(" ");
 
-
-//ラインクロスカウンタ
-  if (tmpc == 0 &&   sensorL < 300 && sensorR < 300 && sensorLL < 300 && sensorRR < 300) {  //速度によって調整
-    cross++;
-    //BUZZER入れたが音小さくて聞こえない
-//    tone(BUZZER,1234,100);
+  //*******************************************************************
+  //ラインカウンタ制御  
+  //*******************************************************************
+  //クロスラインを検知したら、クロスフラグをON
+  if (tmpc == 0 && sensorL < 300 && sensorR < 300 && sensorLL < 300 && sensorRR < 300) {
+    temp_distance = NowDistance;
+    cross = 1;
     tmpc = 1;
-  }
-  if (sensorLL > 700 && sensorRR > 700) {
+    }
+  if ( tmpc == 1 && sensorLL > 700 && sensorRR > 700) {
     tmpc = 0;
   }
 
-  //ゴールセンサーカウンタ
+  //ゴールセンサが検知したらtmpフラグをON
   if (tmp == 0 && sensorGoal < 300 ){  //速度によって調整  && sensorRR > 500 && sensorLL > 500 && Curve > 800
-    count++;
+    if(cross != 1){
+      count++;
+      //BUZZER入れたが音小さくて聞こえない
+      tone(BUZZER,1178,100);
+      // StepSW = !StepSW;
+    }
     tmp = 1;
-    count = count - cross;  //クロスの分をカウントしないようにクロスの部分を通った時に引く
-    cross = cross - cross;
-    //BUZZER入れたが音小さくて聞こえない
-//    tone(BUZZER,1178,100);
   }
-  if (sensorGoal > 700) {
+  //ゴールセンサが検知しなくなったらクロスフラグを確認し、ゴールマーカーを追加するか判断
+  if (tmp == 1 && sensorGoal > 700) {
     tmp = 0;
   }
-  // static uint8_t temp_count = 0; 
-  // static bool ignore = 0;
-  // // //カーブセンサ
-  // if (Curve < 300 && ignore == 0){  //速度によって調整  && sensorRR > 500 && sensorLL > 500 && Curve > 800
-  //   // pgain = 3.0;
-  //   // pgain = 0.17;
-  //   // tmp = 1;
-  //   // curve_count = curve_count - cross;  //クロスの分をカウントしないようにクロスの部分を通った時に引く
-  //   // cross = cross - cross;
-  //   temp_count++;
-  //   if (sensorGoal < 300) {
-  //     ignore = 1;
-  //   }    
-  //   //BUZZER入れたが音小さくて聞こえない
-  // //tone(BUZZER,1178,100);
-  // }
-  // //30ms以内に反対側のセンサが反応しないので、カーブマーカーと認識
-  // if(temp_count >= 2){
-  //   curve_count = ! curve_count;
-  //   temp_count = 0;
-  // }
-  // //マーカーを抜けたら、カウント、ignoreをリセット
-  // if(Curve > 700){
-  //   temp_count = 0;
-  //   ignore = 0;
-  // }
-  // //カーブ中なら、ゲインを高くする。
-  // if(curve_count == 1){
-  //   pgain = 0.3;
-  // }else{
-  //   pgain = 0.16;
-  // }
 
-  SP=SP + 5;
-  if(SP>900.0){
-    SP=900.0;
+  //カーブマーカー検知
+  if (curve_temp == 0  && sensorCurve < 300){
+      //クロスではない場合
+      if(cross != 1){
+        curve_count++;      
+        //BUZZER入れたが音小さくて聞こえない
+        tone(BUZZER,1178,100);
+        }
+      curve_temp = 1;
   }
   
-  //ゴール後少し進んで停止
-  if (count == 2) { 
+  if (sensorCurve > 700) { 
+    curve_temp = 0;
+  }
+  //クロス検知後から35mm進んだらおそらくゴール、カーブセンサは過ぎてるだろうからクロスフラグを戻す（誤検知する場合は調整する）
+  if((NowDistance-temp_distance) >= 43){
+    cross = 0;
+  }
+
+  //ゴール後処理
+  if (count == 2) {
     if (b == 0) {
-      //タイマースタート処理
-      ITimer0.stopTimer();
-      ITimer0.attachInterrupt(inputL, TimerHandler0);  //左モーター
+      //最後のマーカからゴールまでの距離を保存する
+      // marker_distance[curve_count] = NowDistance - tmp_distance;
+      // //タイマースタート処理
+      // ITimer0.stopTimer();
+      // ITimer0.attachInterrupt(100, callback0);  //自宅コースはスタートエリアが狭いため一時的に無効化し即停止するようにしている
+      interval_tar = 0;
+      first_count = curve_count;
       Step = 0;
     }
     b=1;
     
   }
-  if (distance > 100) {  //停止位置は試走会で調整
+  if (count >= 2 && interval < 10) {  //停止位置は試走会で調整
       ITimer0.stopTimer();
+      ITimer1.stopTimer();
       Reset();
-      tone(BUZZER,1446,1000); 
-      
       Run = 0;
+
+      tone(BUZZER,1446,1000); 
+    }
+  //ライン制御
+  // Add_SensorL = sensorL + sensorLL;
+  // Add_SensorR = sensorR + sensorRR;
+
+  //加減速モード（基本的に800以上のところで加減速することになる。）
+  if(marker_distance[curve_count] > 1000){
+    pgain = 0.1;
+    static bool first = 0;
+    if(first == 0){
+    temp_distance2 = NowDistance;
+    first = 1;
+    }
+    if(prev_curve_count != curve_count){
+      temp_distance2 = NowDistance;
+    }
+    // 記録されている距離ー加速モードに入ってからの距離＝400を切ったら減速していく
+    tmporary =  (marker_distance[curve_count])-(NowDistance - temp_distance2);
+    if(tmporary < 550){
+      interval_tar = 300;
+      // Serial.print("減速開始");
+    }else{
+      interval_tar = 900;
     }
 
-  Add_SensorL = sensorL + sensorLL;
-  Add_SensorR = sensorR + sensorRR;
+    prev_curve_count = curve_count;
+  }
 
   //今回の差分
-  diff = Add_SensorL - Add_SensorR - bias;  //biasは試走会で調整
-  
-  //-50~50なら差分なしとする（）
-  if(diff<=650 && diff>=-650){
+  diff = (sensorL + sensorLL) - (sensorR + sensorRR) - bias;  //biasは試走会で調整
+
+  //-50~50なら差分なしとする
+  if(diff<=400 && diff>=-400){
     diff=0;
   }
   //  Serial.print("diff:");
   //  Serial.print(diff);
-  sum += (diff*0.01);
+  sum += diff;
   //P制御
   P = diff * pgain;
   //D制御
-  D = (beforediff - diff)/0.01 * dgain;
+  D = (beforediff - diff) * dgain;
   //I制御
   I = sum * igain;
-  //SP350までは制御しない
-  if(SP >= 350){
   //入力速度にPID制御値を代入
-  inputL = SP + (P + D + I);
-  inputR = SP - (P + D + I);
-  }else{
-  inputL = SP;
-  inputR = SP;    
+  //Dを-にしてPで発生するオーバーシュートを抑える形にすることも検討
+  PID_Result = P + D + I;
+  // inputR = SP - (P + D + I);
+
+// 加減速時のみPIDの値に制限をかける
+if(pgain == 0.1){
+  if(PID_Result < -200){
+    PID_Result = -200;
   }
-  //前回の差分を保存
+  if(PID_Result > 200){
+    PID_Result = 200;
+  }
+}
+  //前回の差分を保存き
   beforediff = diff;
 
-  //最大値補正
-  if (inputL > (SP+200)) {
-    inputL = SP+200;
-  }
-  if (inputR > (SP+200)) {
-    inputR = (SP+200);
-  }
-  //最小値補正
-  if (inputL < 0) {
-    inputL = 0;
-  }
-  if (inputR < 0) {
-    inputR = 0;
-  }
-
   //クロス通過時は速度を固定する
-  if (sensorLL < 500 && sensorRR < 500) {
-    inputL = SP;
-    inputR = SP;
+  if (sensorLL < 400 && sensorRR < 400) {
+    PID_Result = 0;
   }
 
-  //速度設定 Slice1＝Reft Slice2=Left
-  pwm_set_wrap(pwm_slice1, Hz_wrap(inputR));
-  pwm_set_wrap(pwm_slice2, Hz_wrap(inputL));
 
-  //PWMスタート
-  pwm_set_enabled(pwm_slice1, true);
-  pwm_set_enabled(pwm_slice2, true);
 
-  // Serial.print(" P:");
-  // Serial.print(P);
-  // Serial.print(" D:");
-  // Serial.print(D);
-  // Serial.print(" I:");
-  // Serial.print(I);
-  // Serial.print(" inputL:");
-  // Serial.print(inputL);
-  // Serial.print(" inputR:");
-  // Serial.print(inputR);
-  // Serial.print(" count:");
-  // Serial.print(count);
-  // Serial.print(" cross:");
-  // Serial.print(cross);
-  // Serial.print(" tmp:");
-  // Serial.print(distance);
-  // Serial.print(" SP:");
-  // Serial.println(SP);
-
-  //ログ保存(スタートラインを越えてからゴールするまで)
-  // if(count>=1 && count < 2){
-    data_log[log_count].Curve_log = inputL;
-    data_log[log_count].LL_log = sensorLL;
-    data_log[log_count].L_log = sensorL;
-    data_log[log_count].R_log = sensorR;
-    data_log[log_count].RR_log = sensorRR;
-    data_log[log_count].Goal_log = inputR;
+  //ログ保存スタートボタンを押してから
+    // data_log[log_count].Curve_log = sensorCurve;
+    // data_log[log_count].LL_log = curve_count;
+    // data_log[log_count].L_log = NowDistance;
+    // data_log[log_count].R_log = cross;
+    // data_log[log_count].RR_log = temp_distance;
+    // data_log[log_count].Goal_log = count;
 //    data_log[log_count].R_motor_log = inputR;
-//    data_log[log_count].L_motor_log = inputL;
+//    data_log[log_count].L_motor_log = PID_Result;
+    // log_count++;  
     
-    log_count++;  
-    // }
-  
 }
